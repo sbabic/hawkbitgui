8844f0267afc055b84909eaca67a5917492b83e8
Daniel Garcia <soul.daniel@nabladev.com>
2025-03-03 19:05:47 -0500

    feat: Adde login page

---
+++ src/app/login/components/login-form/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import styles from "./styles.module.scss";
+
+type FormValues = {
+    username: string;
+    password: string;
+};
+
+export interface LoginFormProps {
+    className?: string;
+};
+
+export default function LoginForm(props: LoginFormProps) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormValues>();
+
+    const onSubmit = (data: FormValues) => {
+        console.log("Login Data:", data);
+        // Handle authentication logic here
+    };
+
+    return (
+        <div className={`${styles.container} ${props.className}` }>
+            <div className={styles.card}>
+                <h2 className={styles.title}>Login to Account</h2>
+                <p className={styles.subtitle}>
+                    Please enter your username and password to continue
+                </p>
+                <div style={{height: 40}}></div>
+                <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+                    <div className={styles["input-group"]}>
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            {...register("username", { required: "Username is required" })}
+                        />
+                        {errors.username && <p className={styles.error}>{errors.username.message}</p>}
+                    </div>
+                    <div style={{height: 40}}></div>
+
+                    <div className={styles["input-group"]}>
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            {...register("password", { required: "Password is required" })}
+                        />
+                        {errors.password && <p className={styles.error}>{errors.password.message}</p>}
+                    </div>
+                    <div style={{height: 40}}></div>
+                    <button type="submit" className={styles.button}>
+                        Sign In
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,46 @@
+import LoginForm from './components/login-form';
+import styles from './styles.module.scss';
+import Image from "next/image";
+
+export default function Login() {
+    return (
+        <div className={`${styles.page}`}>
+            <div className={`${styles.logoSection}`}>
+                <div className={`${styles.logoContainer}`}>
+                    <Image width={308} height={142} className={styles.logo} alt={'Hawkbit Logo'} src={'/images/hawkbit-logo.svg'}/>
+                </div>
+                <div className={styles.policiesSection}>
+                    <div>
+                        <a href={'https://www.eclipse.org/legal/privacy/'}>Privacy Policy</a>
+                        <p>|</p>
+                        <a href={'https://www.eclipse.org/legal/terms-of-use/'}>Terms of Use</a>
+                        <p>|</p>
+                        <a href={'https://www.eclipse.org/legal/copyright/'}>Copyright Agent</a>
+                    </div>
+                    <div>
+                        <a href={'https://www.eclipse.org/legal/epl-2.0/'}>Eclipse Public License</a>
+                        <p>|</p>
+                        <a href={'https://www.eclipse.org/legal/'}>Legal Resources</a>
+                    </div>
+                    <div>
+                        <p>Copyright © Eclipse Foundation, Inc. All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+            <div className={`${styles.loginSection}`}>
+                <LoginForm className={styles.form}/>
+                <div className={styles.policiesSection}>
+                    <div>
+                        <a href={'https://www.eclipse.org/legal/epl-2.0/'}>About Us</a>
+                        <p>|</p>
+                        <a href={'https://www.eclipse.org/legal/epl-2.0/'}>Contact Us</a>
+                        <p>|</p>
+                        <a href={'https://www.eclipse.org/legal/epl-2.0/'}>Donate</a>
+                        <p>|</p>
+                        <a href={'https://www.eclipse.org/legal/'}>Governance</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
