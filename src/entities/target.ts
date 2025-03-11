fcaf5d75061e8e8213ca4ed046c9348af3c1907b
Daniel Garcia <soul.daniel@nabladev.com>
2025-03-11 18:44:14 -0500

    feat: Added target entity

---
+++ src/entities/target.ts
@@ -0,0 +1,9 @@
+export interface Target {
+    name: string;
+    controllerId: string;
+    description?: string;
+    createdBy: string;
+    createdDate: Date;
+    modifiedBy: string;
+    modifiedDate: Date;
+}
