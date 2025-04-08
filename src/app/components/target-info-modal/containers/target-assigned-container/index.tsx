843bb61005a3a301df517322ee27f61eea5cefe3
Daniel Garcia <soul.daniel@nabladev.com>
2025-04-08 12:19:57 -0500

    feat: Added TargetAssignedContainer component

---
+++ src/app/components/target-info-modal/containers/target-assigned-container/index.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useTargetsTableStore } from '@/stores/targets-table-store';
+import { useEffect, useState } from 'react';
+import { TargetsService } from '@/services/targets-service';
+import TargetAssigned from '@/app/components/target-info-modal/components/target-assigned';
+import { Distribution } from '@/entities';
+
+export default function TargetAssignedContainer() {
+    const selectedTarget = useTargetsTableStore(
+        (state) => state.selectedTarget
+    );
+    const [assignedDistribution, setAssignedDistribution] = useState<
+        Distribution | undefined
+    >();
+
+    useEffect(() => {
+        if (!selectedTarget) return;
+
+        const fetchAssignedDistribution = async () => {
+            const attributes = await TargetsService.getAssignedDistribution(
+                selectedTarget.controllerId
+            );
+            setAssignedDistribution(attributes);
+        };
+
+        fetchAssignedDistribution();
+    }, [selectedTarget]);
+
+    return (
+        <TargetAssigned
+            type={assignedDistribution?.typeName}
+            version={assignedDistribution?.version}
+            name={assignedDistribution?.name}
+        />
+    );
+}
--- src/app/components/target-info-modal/index.tsx
+++ src/app/components/target-info-modal/index.tsx
@@ -1,9 +1,9 @@
 'use client';
 import CardWithSidebar from '@/app/components/card-with-sidebar';
-import TargetAssigned from '@/app/components/target-info-modal/components/target-assigned';
 import TargetDetailsContainer from '@/app/components/target-info-modal/containers/target-details-container';
 import TargetAttributesContainer from '@/app/components/target-info-modal/containers/target-attributes-container';
 import TargetInstalledContainer from '@/app/components/target-info-modal/containers/target-installed-container';
+import TargetAssignedContainer from '@/app/components/target-info-modal/containers/target-assigned-container';
 
 export default function TargetInfo() {
     return (
@@ -14,7 +14,7 @@ export default function TargetInfo() {
                     title: 'Attributes',
                     component: <TargetAttributesContainer />,
                 },
-                { title: 'Assigned', component: <TargetAssigned /> },
+                { title: 'Assigned', component: <TargetAssignedContainer /> },
                 { title: 'Installed', component: <TargetInstalledContainer /> },
                 { title: 'Tags', component: <div>Tags</div> },
                 { title: 'Metadata', component: <div>Metadata</div> },
