a342468787b608ad7d8178ba79eea83ca9f42345
Daniel Garcia <soul.daniel@nabladev.com>
2025-04-09 13:04:26 -0500

    feat: Added TargetMetadataTable component

---
+++ src/app/components/target-info-modal/components/target-metadata/index.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import ListWithTitle from '@/app/components/list-with-title';
+
+export default function TargetMetadata({
+    name,
+    version,
+    type,
+}: {
+    name?: string;
+    version?: string;
+    type?: string;
+}) {
+    const items = [
+        { title: 'Name', value: name },
+        { title: 'Version', value: version },
+        { title: 'Type', value: type },
+    ];
+    return <ListWithTitle title={'Installed'} items={items} />;
+}
