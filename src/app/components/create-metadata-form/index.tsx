c1a7b7eedc42d84f939ae1c3f2f7f4f727049ffe
Daniel Garcia <soul.daniel@nabladev.com>
2025-05-27 07:02:51 -0500

    fix: Now save button is triggered when pressed in CreateMetadataForm component

--- src/app/components/create-metadata-form/index.tsx
+++ src/app/components/create-metadata-form/index.tsx
@@ -7,45 +7,45 @@ import Input from '../input';
 import TextArea from '@/app/components/text-area';
 
 interface FormData {
-    key: string;
-    value: string;
+  key: string;
+  value: string;
 }
 
 export default function CreateMetadataForm({ onSubmit, onCancel }: { onSubmit: (data: FormData) => void; onCancel?: () => void }) {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-    return (
-        <div className={styles.container}>
-            <h3>Create Metadata</h3>
-            <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-                <div className={styles.inputContainer}>
-                    <label className={styles.label}>
-                        Key <b className={styles.error}>*</b>
-                    </label>
-                    <Input
-                        className={styles.input}
-                        type='text'
-                        placeholder='Key'
-                        {...register('key', { required: true })}
-                        error={errors.key && 'This field is required'}
-                    />
-                </div>
-                <div className={styles.inputContainer}>
-                    <label className={styles.label}>Value</label>
-                    <TextArea placeholder={'Key'} {...register('value')} className={styles.textarea} error={errors.value && 'This field is required'} />
-                </div>
-
-                <div className={styles.buttonContainer}>
-                    <Button>Save</Button>
-                    <Button variant='outline' onClick={onCancel}>
-                        Cancel
-                    </Button>
-                </div>
-            </form>
+  return (
+    <div className={styles.container}>
+      <h3>Create Metadata</h3>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <div className={styles.inputContainer}>
+          <label className={styles.label}>
+            Key <b className={styles.error}>*</b>
+          </label>
+          <Input
+            className={styles.input}
+            type='text'
+            placeholder='Key'
+            {...register('key', { required: true })}
+            error={errors.key && 'This field is required'}
+          />
         </div>
-    );
+        <div className={styles.inputContainer}>
+          <label className={styles.label}>Value</label>
+          <TextArea placeholder={'Key'} {...register('value')} className={styles.textarea} error={errors.value && 'This field is required'} />
+        </div>
+
+        <div className={styles.buttonContainer}>
+          <Button type='submit'>Save</Button>
+          <Button variant='outline' onClick={onCancel}>
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/app/components/target-info-modal/containers/target-metadata-container/index.tsx
+++ src/app/components/target-info-modal/containers/target-metadata-container/index.tsx
@@ -9,27 +9,27 @@ import { useTargetsTableStore } from '@/stores/targets-table-store';
 import { useTargetsMetadataTableStore } from '@/stores/targets-metadata-table-store';
 
 export default function TargetMetadataContainer() {
-    const selectedTarget = useTargetsTableStore((state) => state.selectedTarget);
-    const fetchMetadata = useTargetsMetadataTableStore((state) => state.fetchMetadata);
-    const [isCreateMetadataModalOpen, setIsCreateMetadataModalOpen] = useState<boolean>(false);
+  const selectedTarget = useTargetsTableStore((state) => state.selectedTarget);
+  const fetchMetadata = useTargetsMetadataTableStore((state) => state.fetchMetadata);
+  const [isCreateMetadataModalOpen, setIsCreateMetadataModalOpen] = useState<boolean>(false);
 
-    const handleOnAddClick = () => {
-        setIsCreateMetadataModalOpen(true);
-    };
+  const handleOnAddClick = () => {
+    setIsCreateMetadataModalOpen(true);
+  };
 
-    const handleOnSubmit = async (data: { key: string; value: string }) => {
-        if (!selectedTarget) return;
-        await TargetsService.createMetadata(selectedTarget.controllerId, data);
-        await fetchMetadata(selectedTarget.controllerId);
-        setIsCreateMetadataModalOpen(false);
-    };
+  const handleOnSubmit = async (data: { key: string; value: string }) => {
+    if (!selectedTarget) return;
+    await TargetsService.createMetadata(selectedTarget.controllerId, data);
+    await fetchMetadata(selectedTarget.controllerId);
+    setIsCreateMetadataModalOpen(false);
+  };
 
-    return (
-        <>
-            <TargetMetadata onAddClick={handleOnAddClick} />
-            <Modal isOpen={isCreateMetadataModalOpen} onClose={() => setIsCreateMetadataModalOpen(false)} size={'fitContent'}>
-                <CreateMetadataForm onSubmit={handleOnSubmit} />
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <TargetMetadata onAddClick={handleOnAddClick} />
+      <Modal isOpen={isCreateMetadataModalOpen} onClose={() => setIsCreateMetadataModalOpen(false)} size={'fitContent'}>
+        <CreateMetadataForm onSubmit={handleOnSubmit} />
+      </Modal>
+    </>
+  );
 }
