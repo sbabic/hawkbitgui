69f9152b042993bb75333b7f9b39ce7736d84833
Xavier Garcia <xavicogarcia@nabladev.com>
2025-07-04 10:26:14 -0500

    fix: Avoid infinite re renders of table when visibility is not present
    Signed-off-by: Xavier Garcia <xavicogarcia@nabladev.com>

--- src/app/components/table/index.tsx
+++ src/app/components/table/index.tsx
@@ -6,6 +6,7 @@ import Skeleton from 'react-loading-skeleton';
 import styles from './styles.module.scss';
 import DraggableDroppableRow from '@/app/components/draggable-droppable-row';
 import { isEqual } from 'lodash-es';
+import { isDefined } from '@/utils/is-defined';
 
 export type TableProps<T> = {
   data: T[];
@@ -50,8 +51,8 @@ export default function Table<T>({
   });
 
   useEffect(() => {
-    if (!isEqual(columnVisibility, defaultColumnVisibility)) {
-      setColumnVisibility(defaultColumnVisibility ?? {});
+    if (isDefined(defaultColumnVisibility) && !isEqual(columnVisibility, defaultColumnVisibility)) {
+      setColumnVisibility(defaultColumnVisibility);
     }
   }, [columnVisibility, defaultColumnVisibility]);
 
