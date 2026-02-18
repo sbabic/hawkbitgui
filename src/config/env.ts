06261fa1db94ffb416000d51596b2fbb64e56e28
Bob van de Vijver <bobvandevijver@hotmail.com>
2026-02-18 21:42:37 +0100

    Remove NEXT_PUBLIC prefix from HAWKBIT_API_URL env name

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const environment = {
     nextAuthSecret: process.env.NEXTAUTH_SECRET,
     nextAuthUrl: process.env.NEXTAUTH_URL,
-    hawkbitApiUrl: process.env.NEXT_PUBLIC_HAWKBIT_API_URL,
+    hawkbitApiUrl: process.env.HAWKBIT_API_URL,
 };
