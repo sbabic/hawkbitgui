a21a72c760abd8b3b49be723d14bb72bb000c447
Oleksandr Andrushchenko <andr2000@gmail.com>
2026-03-05 16:02:41 +0200

    Allow uploading artifacts bigger than 10MiB
    By default Next.js only allows uploading files up to 10MiB which may not
    be sufficient for bigger swu files [1]. Unfortunately it is not possible to
    increase the allowed size with an environment variable and needs the
    corresponding setting to be hardcoded, so increase the upload size to
    2GiB.
    
    [1] https://nextjs.org/docs/app/api-reference/config/next-config-js/proxyClientMaxBodySize
    
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    experimental: {
+        proxyClientMaxBodySize: '2gb',
+    },
     async headers() {
         return [
             {
